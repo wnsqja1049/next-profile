41d79fabfb3d61c1594a55ca50f0165f43d51f36
wnsqja1049 <wnsqja1049@gmail.com>
2024-06-26 00:09:43 +0900

    탑버튼 추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,36 @@ import { AboutMe } from "@/components/about-me";
 import { Career } from "@/components/career";
 import { Skills } from "@/components/skills";
 import { Projects } from "@/components/projects";
+import { Button } from "@nextui-org/button";
+import { TopButton } from "@/components/top-button";
 
 export default function Home() {
 
 	return (
-		<div className="flex flex-col items-center gap-20 mb-40">
-			<section id="id_about_me" className="flex flex-col items-center gap-5">
-				<div className="text-5xl font-blackHanSans">ABOUT ME</div>
-				<AboutMe/>
-			</section>
+		<div>
+			<div className="flex flex-col items-center gap-20 mb-40">
+				<section id="id_about_me" className="flex flex-col items-center gap-5">
+					<div className="text-5xl font-blackHanSans">ABOUT ME</div>
+					<AboutMe/>
+				</section>
 
-			<section id="id_career" className="flex flex-col items-center gap-5">
-				<div className="text-5xl font-blackHanSans">CAREER</div>
-				<Career/>
-			</section>
+				<section id="id_career" className="flex flex-col items-center gap-5">
+					<div className="text-5xl font-blackHanSans">CAREER</div>
+					<Career/>
+				</section>
 
-			<section id="id_skills" className="flex flex-col items-center gap-5">
-				<div className="text-5xl font-blackHanSans">SKILLS</div>
-				<Skills/>
-			</section>
+				<section id="id_skills" className="flex flex-col items-center gap-5">
+					<div className="text-5xl font-blackHanSans">SKILLS</div>
+					<Skills/>
+				</section>
 
-			<section id="id_projects" className="flex flex-col items-center gap-5">
-				<div className="text-5xl font-blackHanSans">PROJECTS</div>
-				<Projects/>
-			</section>
+				<section id="id_projects" className="flex flex-col items-center gap-5">
+					<div className="text-5xl font-blackHanSans">PROJECTS</div>
+					<Projects/>
+				</section>
+			</div>
+			
+			<TopButton/>
 		</div>
 	);
 }
---
+++ components/top-button.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+import { Image, Button } from "@nextui-org/react";
+
+export const TopButton = () => {
+  const [hideButton, setHideButton] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (document.documentElement.scrollTop > 1000) {
+      setHideButton(false);
+    } else {
+      setHideButton(true);
+    }
+  };
+
+  return (
+    <>
+      {hideButton ? <></> : 
+      <Button isIconOnly  className="sticky bottom-10 float-right bg-transparent border-2 border-black dark:border-white" radius="full"
+        onPress={() => {
+          var anchor = document.querySelector(`#id_about_me`)
+          anchor!.scrollIntoView({ behavior: 'smooth', block: 'center' })
+        }}>
+          <div className="text-4xl font-bold">↑</div>
+          </Button>}
+    </>
+  );
+};
